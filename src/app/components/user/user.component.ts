f35f4959b4bebb1d822943ac9bc9c8555a648a58
missprempree <delenies@gmail.com>
2024-12-17 12:21:49 +0700

    Update User Form input

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,11 +1,22 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
+  // Binding the following objects to Input Form
+  userObj: any = {
+    firstName: '',
+    lastName: '',
+    userName: '',
+    city: '',
+    state: '',
+    zipCode: '',
+    isTermsAgree: false
+  }
 
 }
